b6dca176bdeb4ad632618507f884b269d09b664a
FaramarzAf <faramarzafzali@gmail.com>
2019-10-28 13:19:11 +0330

    OK right now

--- app/src/main/java/com/faramarz/tictacdev/permission/MainActivity.java
+++ app/src/main/java/com/faramarz/tictacdev/permission/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,8 +17,37 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btnCamera, btnWriteExternalStorage, btnAccessFineLocation, btnContacts;
-    public static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
-    List<String> listPermissionsNeeded = new ArrayList<>();
+
+    public static final int MY_PERMISSIONS_REQUEST_CONTACT = 2;
+    public static final int MY_PERMISSIONS_REQUEST_CAMERA = 3;
+    public static final int MY_PERMISSIONS_REQUEST_STORAGE = 4;
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 5;
+
+
+    /*
+    FOR FRAGMENT
+
+if you are trying this code in a fragment, change
+
+checkSelfPermission()
+
+to
+
+ActivityCompact.checkSelfPermission()
+
+and Also change
+
+ActivityCompat.requestPermissions()
+
+to
+
+requestPermissions()
+
+Handling of Permission Result (Allow or Deny) are same as activity.
+
+For a More Complete Example See This Answer Here
+     */
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,43 +55,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         bind();
         clickEvents();
-    }
-
-    private boolean checkCameraPermission() {
-        int camera = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA);
-        if (camera != PackageManager.PERMISSION_GRANTED) {
-            listPermissionsNeeded.add(android.Manifest.permission.CAMERA);
-            multiplePermission();
-        }
-        return true;
-    }
-
-    private boolean checkStoragePermission() {
-        int storage = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        if (storage != PackageManager.PERMISSION_GRANTED) {
-            listPermissionsNeeded.add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
-            multiplePermission();
-        }
-        return true;
-    }
-
-    private boolean checkFineLocationPermission() {
-        int loc = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
-        if (loc != PackageManager.PERMISSION_GRANTED) {
-            listPermissionsNeeded.add(Manifest.permission.ACCESS_FINE_LOCATION);
-            multiplePermission();
-        }
-        return true;
-    }
-
-    private boolean checkContactsPermission() {
-        int contacts = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS);
-        if (contacts != PackageManager.PERMISSION_GRANTED) {
-            listPermissionsNeeded.add(Manifest.permission.READ_CONTACTS);
-            multiplePermission();
-        }
-        return true;
-
     }
 
     private void bind() {
@@ -78,34 +71,117 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnAccessFineLocation.setOnClickListener(this);
     }
 
-    private boolean multiplePermission() {
-        if (!listPermissionsNeeded.isEmpty()) {
-            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), REQUEST_ID_MULTIPLE_PERMISSIONS);
-            return false;
-        }
-        return true;
-    }
-
     @Override
     public void onClick(View v) {
         int id = v.getId();
         switch (id) {
+
             case R.id.btnCamera:
                 checkCameraPermission();
                 break;
+
             case R.id.btnWriteExternalStorage:
                 checkStoragePermission();
                 break;
+
             case R.id.btnAccessFineLocation:
-                checkFineLocationPermission();
+                checkLocationPermission();
                 break;
+
             case R.id.btnContacts:
-                checkContactsPermission();
+                checkContactPermission();
                 break;
 
             default:
                 break;
         }
+
+    }
+
+
+    void checkContactPermission() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            // No explanation needed, we can request the permission.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, MY_PERMISSIONS_REQUEST_CONTACT);
+            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
+        }
+
+    }
+
+    void checkCameraPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
+        }
+
+    }
+
+    void checkStoragePermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_STORAGE);
+        }
+    }
+
+
+    void checkLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CONTACT: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Toast.makeText(getApplicationContext(), "Contact granted", Toast.LENGTH_SHORT).show();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(getApplicationContext(), "Contact denied", Toast.LENGTH_SHORT).show();
+                }
+                return;
+
+            }
+            case MY_PERMISSIONS_REQUEST_CAMERA: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Camera granted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Camera denied", Toast.LENGTH_SHORT).show();
+                }
+                return;
+
+            }
+
+            case MY_PERMISSIONS_REQUEST_STORAGE: {
+
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "Storage granted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Storage denied", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "Location granted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Location denied", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+        }
+
 
     }
 
